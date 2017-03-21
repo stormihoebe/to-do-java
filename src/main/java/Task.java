150341a2216253c27933b228550f3193bdc5f339
Gloria Friesen and Stormi Hoebelheinrich <gloria@>
2017-03-21 09:24:20 -0700

    add application to track to-do list with spark and velocity

---
+++ src/main/java/Task.java
@@ -0,0 +1,10 @@
+public class Task {
+  private String mDescription;
+
+  public Task(String description) {
+    mDescription = description;
+  }
+  public String getDescription(){
+    return mDescription;
+  }
+}
